68d59d4812f1aec535e627a09b0f70c7c313c92d
Korvo2Tuirow <korvo2irow@live.com>
2023-03-03 08:31:00 -0300

    maisCanais

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 let canais = [
   {
-    id: 1,
     nome: "Globo",
     url: "https://br.embedflix.net/p2p/globosp_720p/chunks.m3u8",
+    img: "https://tvonline.app/wp-content/uploads/2022/11/93cb68732da806c8532f8e24cd013f4d.jpg"
   },
-
+  
   {
-    id: 2,
-    nome: "History",
-    url: "https://live.embedflix.net/p2p/history/chunks.m3u8",
+    nome: "BBB",
+    url: "https://br.embedflix.net/p2p/bbb/chunks.m3u8",
+    img: "https://tvonline.app/wp-content/uploads/2023/01/20230103115255888490a.jpg"
   },
 
   {
     id: 3,
-    nome: "BBB",
-    url: "https://br.embedflix.net/p2p/bbb/chunks.m3u8",
+    nome: "History",
+    url: "https://live.embedflix.net/p2p/history/chunks.m3u8",
+    img: "https://tvonline.app/wp-content/uploads/2022/11/k-33_Lq2_400x400.png"
   },
+
+  {
+    nome: "History 2",
+    url: "https://live.embedflix.net/p2p/h2/chunks.m3u8",
+    img: "https://tvonline.app/wp-content/uploads/2022/11/10229_history2_1-e1668191421447.jpg"
+  },
+
+  {
+    nome: "Combate",
+    url: "https://live.embedflix.net/p2p/combate/chunks.m3u8",
+    img: "https://tvonline.app/wp-content/uploads/2022/11/91dccc0cfa4c92f24f8e8f4d9fd6e3e1.jpg"
+  },
+
+
 ];
 
 const c = (el) => document.querySelector(el);
@@ -24,9 +39,10 @@ const videoPlayer = document.querySelector(".videoPlayer");
 canais.map((item, index) => {
   let buttons = c(".model .modelBtn").cloneNode(true);
 
-  buttons.setAttribute("data-key", index);
-
+  buttons.setAttribute("data-key",index);
   buttons.querySelector(".btn").innerHTML = item.nome;
+  buttons.querySelector('.btn').style.backgroundImage = `url("${item.img}")`;
+  buttons.querySelector('.btn') 
 
   buttons.querySelector(".btn").addEventListener("click", (e) => {
     e.preventDefault();
