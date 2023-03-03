22d5035899bfd49af786ae7994faaa60bc210315
Korvo2Tuirow <korvo2irow@live.com>
2023-03-02 21:25:44 -0300

    commit_globo

---
+++ script.js
@@ -0,0 +1,37 @@
+let canais = [
+  {
+    canal: 01,
+    nome: "Globo",
+    url: "https://br.embedflix.net/p2p/globosp_720p/chunks.m3u8",
+  },
+
+  {
+    canal: 02,
+    nome: "History",
+    url: "https://br.embedflix.net/p2p/globosp_720p/chunks.m3u8",
+  },
+  
+
+];
+
+const c = (el)=>document.querySelector(el);
+
+canais.map((item)=>{
+
+    let buttons = c(".modelBtn").cloneNode(true);
+
+    buttons.querySelector(".btn").innerHTML = item.nome;
+
+    
+
+    c('.btns').append(buttons);
+
+})
+
+
+//let btn = document.querySelector("button").addEventListener('click', ()=>{
+
+
+
+//})
+
